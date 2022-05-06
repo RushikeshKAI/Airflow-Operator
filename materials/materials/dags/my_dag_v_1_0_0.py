55d6033be9388570cfe042c647bc490c9e77e17e
Rushikesh <rushikesh.mahadik@konverge.ai>
2022-05-06 17:59:27 +0530

    dags updated

--- materials/materials/dags/my_dag_v_1_0_0.py
+++ materials/materials/dags/my_dag_v_1_0_0.py
@@ -3,6 +3,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
+from airflow.utils.helpers import cross_downstream
 
 
 default_args = {
@@ -15,27 +16,55 @@ def _my_func(execution_date):
     if execution_date.day == 9:
         raise ValueError("Error")
 
-with DAG('my_dag_v_1_0_0', default_args=default_args, start_date=datetime(2022, 5, 6), schedule_interval='@daily', dagrun_timeout=40, catchup=True) as dag:
 
-    task_a = BashOperator(
-        owner='a',
-        task_id='task_a',
-        bash_command='echo "task_a" && sleep 10'
+with DAG('my_dag_v_1_0_0', default_args=default_args, start_date=datetime(2022, 5, 6), schedule_interval='@daily', catchup=False) as dag:
+
+    extract_a = BashOperator(
+        owner='extract_a',
+        task_id='extract_a',
+        bash_command='echo "task_a" && sleep 10',
+        wait_for_downstream=True
     )
 
-    task_b = BashOperator(
-        owner='b',
-        task_id='task_b',
+    extract_b = BashOperator(
+        owner='extract_b',
+        task_id='extract_b',
+        bash_command='echo "task_a" && sleep 10',
+        wait_for_downstream=True
+    )
+
+    process_a = BashOperator(
+        owner='process_a',
+        task_id='process_a',
         retries=3,
         retry_exponential_backoff=True,
         retry_delay=timedelta(seconds=10),
-        bash_command='echo "{{ti.try_number}} && exit 0"'
+        bash_command='echo "{{ti.try_number}} && sleep 20"'
     )
 
-    task_c = PythonOperator(
-        task_id='task_c',
+    process_b = BashOperator(
+        owner='process_b',
+        task_id='process_b',
+        retries=3,
+        retry_exponential_backoff=True,
+        retry_delay=timedelta(seconds=10),
+        bash_command='echo "{{ti.try_number}} && sleep 20"'
+    )
+
+    process_c = BashOperator(
+        owner='process_c',
+        task_id='process_c',
+        retries=3,
+        retry_exponential_backoff=True,
+        retry_delay=timedelta(seconds=10),
+        bash_command='echo "{{ti.try_number}} && sleep 20"'
+    )
+
+    store = PythonOperator(
+        task_id='store',
         python_callable=_my_func,
         depends_on_past=True
     )
 
-    task_a >> task_b >> task_c
+    cross_downstream([extract_a, extract_b], [process_a, process_b, process_c]) >> store
+    # [process_a, process_b, process_c] >> store
